a5ef535dd3e72f7e8e44e26761fbc1adc1470559
ronny781 <ronny1998@gmail.com>
2021-11-13 23:49:39 +0200

    Added BaseAction

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/ronnypc/CLionProjects/Hello
+# Install script for directory: /home/ronnypc/CLionProjects/Assignment1_SPL
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ronnypc/CLionProjects/Hello/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ronnypc/CLionProjects/Assignment1_SPL/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
